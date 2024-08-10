cf1946413880887276467c6b4692d00f2bcc71da
Solomia <mia.bobyak@gmail.com>
2024-08-10 19:39:13 +0300

    added the basic structure of the project

---
+++ src/main/java/box/app/BoxApplication.java
@@ -0,0 +1,11 @@
+package box.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BoxApplication.class, args);
+    }
+}
---
+++ src/test/java/box/app/ApplicationTests.java
@@ -0,0 +1,11 @@
+package box.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
