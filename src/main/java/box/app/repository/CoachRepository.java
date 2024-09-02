8f2d203def646ec2af2bcaf9910796773d639a72
Solomia <mia.bobyak@gmail.com>
2024-09-02 12:17:28 +0300

    add mapper, repository, controller, service

---
+++ src/main/java/box/app/repository/CoachRepository.java
@@ -0,0 +1,9 @@
+package box.app.repository;
+
+import box.app.model.coach.Coach;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoachRepository extends JpaRepository<Coach, Long> {
+}
