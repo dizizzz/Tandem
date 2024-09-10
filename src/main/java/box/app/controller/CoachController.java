48a7bb1bebb288268578fbce0bafafca5146e153
Solomia <mia.bobyak@gmail.com>
2024-09-10 18:07:38 +0300

    added annotation

--- src/main/java/box/app/controller/CoachController.java
+++ src/main/java/box/app/controller/CoachController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Tag(name = "Coach management", description = "Endpoints for coaches managing")
 @RequiredArgsConstructor
 @RestController
