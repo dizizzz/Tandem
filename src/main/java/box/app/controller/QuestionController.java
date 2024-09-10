95aabc516406e288081b4a26691e340d9d747c48
Solomia <mia.bobyak@gmail.com>
2024-09-10 18:08:07 +0300

    added annotation and methods

--- src/main/java/box/app/controller/QuestionController.java
+++ src/main/java/box/app/controller/QuestionController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Tag(name = "Question management", description = "Endpoints for questions managing")
 @RequiredArgsConstructor
 @RestController
@@ -36,16 +38,25 @@ public class QuestionController {
         return questionService.add(requestDto);
     }
 
-    @GetMapping
-    @Operation(summary = "Get all questions", description = "Get a list of all questions")
-    public List<QuestionDto> getAll(Pageable pageable) {
-        return questionService.findAll(pageable);
+    @GetMapping("/eng")
+    @Operation(summary = "Get all English questions",
+            description = "Get a list of all questions in English")
+    public List<QuestionDto> getAllEng(Pageable pageable) {
+        return questionService.findAllEngQuestions(pageable);
     }
 
-    @GetMapping("/{id}")
-    @Operation(summary = "Get the question by ID", description = "Get the question by ID")
-    public QuestionDto getQuestionById(@PathVariable Long id) {
-        return questionService.getById(id);
+    @GetMapping("/ukr")
+    @Operation(summary = "Get all Ukrainian questions",
+            description = "Get a list of all questions in Ukrainian")
+    public List<QuestionDto> getAllUkr(Pageable pageable) {
+        return questionService.findAllUkrQuestions(pageable);
+    }
+
+    @GetMapping("/deu")
+    @Operation(summary = "Get all German questions",
+            description = "Get a list of all questions in German")
+    public List<QuestionDto> getAllDeu(Pageable pageable) {
+        return questionService.findAllDeuQuestions(pageable);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
